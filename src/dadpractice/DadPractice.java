f9eb0fb9873cac02d84a674cc1aa89dfc59c69a3
Nihar Sidhu <niharsidhu@yahoo.com>
2014-01-10 20:11:36 -0500

    stuff

---
+++ src/dadpractice/DadPractice.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package dadpractice;
+
+/**
+ *
+ * @author Nihar
+ */
+public class DadPractice {
+
+    String name;
+    public DadPractice()
+    {
+        name = "Hello my name is bob and I am going to clone this from my dad's repo";
+    }
+}
